fcbbcf92c642f945646e0ec6a62aac867bbec868
Hansel Omondi <ogopabhs@gmail.com>
2024-08-05 12:26:19 +0300

    Change: return type of getBooks endpoint function
    Previously, it was returning a List<Book>. This was incorrect since
    response for the /books endpoint is a JSON string that can only be
    mapped to the domain model BookData. This was changed to BookData, in
    line with the changes made to the BookRepository abstraction

--- app/src/main/java/org/kamilimu/books/viewbooks/data/remote/BookApiService.kt
+++ app/src/main/java/org/kamilimu/books/viewbooks/data/remote/BookApiService.kt
@@ -1,12 +1,13 @@
 package org.kamilimu.books.viewbooks.data.remote
 
 import org.kamilimu.books.viewbooks.domain.model.Book
+import org.kamilimu.books.viewbooks.domain.model.BookData
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface BookApiService {
     @GET("/books")
-    suspend fun getBooks(): List<Book>
+    suspend fun getBooks(): BookData
 
     @GET("/books/{id}")
     suspend fun getBookById(@Path("id") id: Int): Book
