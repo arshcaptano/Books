6de7f87c78a2f32c0254b103e3f26c6288d7eb52
Hansel Omondi <ogopabhs@gmail.com>
2024-08-05 12:20:35 +0300

    Change: getBooks Either.onRight type to BookData
    Previously, the onRight type was Book. This caused an error when the
    retrofit client would try to map the /books response to Book. Books is
    just an item in the JSON object and exists as an array under the key
    "results". This has been changed to BookData, therefore, solving the
    issue

--- app/src/main/java/org/kamilimu/books/viewbooks/domain/repository/BookRepository.kt
+++ app/src/main/java/org/kamilimu/books/viewbooks/domain/repository/BookRepository.kt
@@ -2,10 +2,11 @@ package org.kamilimu.books.viewbooks.domain.repository
 
 import arrow.core.Either
 import org.kamilimu.books.viewbooks.domain.model.Book
+import org.kamilimu.books.viewbooks.domain.model.BookData
 import org.kamilimu.books.viewbooks.domain.model.NetworkError
 
 interface BookRepository {
-    suspend fun getBooks(): Either<NetworkError, List<Book>>
+    suspend fun getBooks(): Either<NetworkError, BookData>
 
     suspend fun getBookById(bookId: Int): Either<NetworkError, Book>
 }
