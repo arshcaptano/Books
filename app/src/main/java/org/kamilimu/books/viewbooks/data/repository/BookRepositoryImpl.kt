6af0f3c98750931cfa7268d2356fc32fb74cf6a3
Hansel Omondi <ogopabhs@gmail.com>
2024-08-05 12:24:46 +0300

    Change: the Either.onRight type in repoImpl
    This was changed to BookData from the previous Book, in line with the
    adjustments made to the BooksRepository abstraction in the domain layer

--- app/src/main/java/org/kamilimu/books/viewbooks/data/repository/BookRepositoryImpl.kt
+++ app/src/main/java/org/kamilimu/books/viewbooks/data/repository/BookRepositoryImpl.kt
@@ -4,6 +4,7 @@ import arrow.core.Either
 import org.kamilimu.books.viewbooks.data.mapper.toNetworkError
 import org.kamilimu.books.viewbooks.data.remote.BookApiService
 import org.kamilimu.books.viewbooks.domain.model.Book
+import org.kamilimu.books.viewbooks.domain.model.BookData
 import org.kamilimu.books.viewbooks.domain.model.NetworkError
 import org.kamilimu.books.viewbooks.domain.repository.BookRepository
 import javax.inject.Inject
@@ -11,7 +12,7 @@ import javax.inject.Inject
 class BookRepositoryImpl @Inject constructor(
     private val bookApiService: BookApiService
 ): BookRepository {
-    override suspend fun getBooks(): Either<NetworkError, List<Book>> {
+    override suspend fun getBooks(): Either<NetworkError, BookData> {
         return Either.catch {
             bookApiService.getBooks()
         }.mapLeft { throwable ->
