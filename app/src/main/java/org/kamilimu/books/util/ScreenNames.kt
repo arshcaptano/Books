3964a28d5c0cd53384f3a29ec194489039d7143c
Hansel Omondi <ogopabhs@gmail.com>
2024-08-16 18:57:54 +0300

    Add: enum class containing ScreenNames
    This enum class contains the names of the app's screens. They will be
    used to display the Screen name in the topAppBar as well as creating
    destinations in the NavGraph in BooksScreen

---
+++ app/src/main/java/org/kamilimu/books/util/ScreenNames.kt
@@ -0,0 +1,7 @@
+package org.kamilimu.books.util
+
+enum class ScreenNames(val title: String) {
+    HomeScreen(title = "Books"),
+    FavouritesScreen(title = "Favourites"),
+    BookDetailsScreen(title = "Book Details")
+}
