0f146889fdb9c647f394701f6de83f32cf4a2723
Hansel Omondi <ogopabhs@gmail.com>
2024-08-17 09:29:30 +0300

    Add: BookmarkViewModel and FavouritesScreen
    The FavouritesScreen displays the Bookmarks that have been added

---
+++ app/src/main/java/org/kamilimu/books/bookmarks/presentation/bookmarkScreen/BookmarkViewModel.kt
@@ -0,0 +1,66 @@
+package org.kamilimu.books.bookmarks.presentation.bookmarkScreen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.kamilimu.books.bookmarks.domain.repository.BookmarkRepository
+import org.kamilimu.books.viewbooks.domain.model.Book
+import org.kamilimu.books.viewbooks.presentation.books_screen.BooksViewState
+import javax.inject.Inject
+
+@HiltViewModel
+class BookmarkViewModel @Inject constructor(
+    private val bookmarkRepository: BookmarkRepository
+) : ViewModel() {
+    private val _bookmarkState = MutableStateFlow<BooksViewState>(BooksViewState.Loading)
+    val bookmarkState: StateFlow<BooksViewState> = _bookmarkState.asStateFlow()
+
+    init {
+        getAllBookmarks()
+    }
+
+    /**
+     * Query for all existing bookmarks in the local database
+     */
+    private fun getAllBookmarks() = viewModelScope.launch {
+        bookmarkRepository.getAllBookmarks().collect { books ->
+            if (books.isEmpty()) {
+                _bookmarkState.update {
+                    BooksViewState.Failure(message = "No bookmarks added")
+                }
+            } else {
+                _bookmarkState.update {
+                    BooksViewState.Success(books)
+                }
+            }
+        }
+    }
+
+    /**
+     * Adds a new book to bookmarks in the local database
+     */
+    fun onAddBookmark(book: Book) = viewModelScope.launch {
+        bookmarkRepository.addNewBookmark(book)
+        getAllBookmarks()  // Update the state after performing a Create0i8
+    }
+
+    /**
+     * Deletes a bookmark from the local database
+     */
+    fun onDeleteBookmark(book: Book) = viewModelScope.launch {
+        bookmarkRepository.deleteBookmark(book)
+        getAllBookmarks()  // Update the state after performing a Delete
+    }
+
+    /**
+     * Retrieves a specific book from the bookmarks stored in the local database
+     */
+    fun getBookmarkById(bookId: Int) = viewModelScope.launch {
+        bookmarkRepository.getBookmarkById(bookId)
+    }
+}
---
+++ app/src/main/java/org/kamilimu/books/bookmarks/presentation/bookmarkScreen/FavouritesScreen.kt
@@ -0,0 +1,83 @@
+package org.kamilimu.books.bookmarks.presentation.bookmarkScreen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.navigation.NavHostController
+import org.kamilimu.books.R
+import org.kamilimu.books.util.ScreenNames
+import org.kamilimu.books.util.components.BookCard
+import org.kamilimu.books.util.components.BooksBottomBar
+import org.kamilimu.books.util.components.BooksTopBar
+import org.kamilimu.books.util.components.ErrorScreen
+import org.kamilimu.books.util.components.LoadingScreen
+import org.kamilimu.books.viewbooks.domain.model.Book
+import org.kamilimu.books.viewbooks.presentation.books_screen.BooksViewState
+
+@Composable
+fun FavouritesScreen(
+    modifier: Modifier = Modifier,
+    bookmarksUiState: BooksViewState,
+    currentScreen: ScreenNames,
+    navController: NavHostController,
+    onAddBookmark: (Book) -> Unit,
+    onRemoveBookmark: (Book) -> Unit
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            BooksTopBar(title = currentScreen.title)
+        },
+        bottomBar = {
+            BooksBottomBar(
+                currentScreen = currentScreen,
+                onHomeClicked = {
+                    navController.navigate(ScreenNames.HomeScreen.name)
+                },
+                onFavoritesClicked = {
+                    navController.navigate(ScreenNames.FavouritesScreen.name)
+                }
+            )
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            when (bookmarksUiState) {
+                is BooksViewState.Loading -> {
+                    LoadingScreen(modifier = Modifier.fillMaxSize())
+                }
+
+                is BooksViewState.Failure -> {
+                    ErrorScreen(
+                        message = bookmarksUiState.message,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+
+                is BooksViewState.Success -> {
+                    LazyColumn {
+                        items(bookmarksUiState.books) { book ->
+                            BookCard(
+                                book = book,
+                                navController = navController,
+                                onFavouriteClicked = {
+                                    if (book.isBookmarked)
+                                        onRemoveBookmark(book) else onAddBookmark(book)
+                                },
+                                onCardClicked = {},
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(dimensionResource(R.dimen.padding_medium))
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
